c570e5a8512153c92e7a48a6f43fe5ac5a92b6cb
Viliam Gago <viliam.gago95@gmail.com>
2025-11-22 10:19:53 +0100

    terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,23 +38,23 @@ locals {
   # App resource group (where Container App lives)
   app_rg_name     = data.azurerm_resource_group.app_rg.name
   app_rg_location = data.azurerm_resource_group.app_rg.location
-  
+
   # Core infrastructure references (from fin_az_core remote state)
-  storage_account_name     = data.terraform_remote_state.core_infra.outputs.storage_account_name
-  storage_account_url      = data.terraform_remote_state.core_infra.outputs.storage_account_url
-  storage_account_id       = data.terraform_remote_state.core_infra.outputs.storage_account_id
-  key_vault_url            = data.terraform_remote_state.core_infra.outputs.key_vault_url
-  key_vault_name           = data.terraform_remote_state.core_infra.outputs.key_vault_name
-  key_vault_id             = data.terraform_remote_state.core_infra.outputs.key_vault_id
-  acr_login_server         = data.terraform_remote_state.core_infra.outputs.container_registry_login_server
-  acr_id                   = data.terraform_remote_state.core_infra.outputs.container_registry_id
-  storage_container_name   = data.terraform_remote_state.core_infra.outputs.storage_container_raw_name
-  
+  storage_account_name   = data.terraform_remote_state.core_infra.outputs.storage_account_name
+  storage_account_url    = data.terraform_remote_state.core_infra.outputs.storage_account_url
+  storage_account_id     = data.terraform_remote_state.core_infra.outputs.storage_account_id
+  key_vault_url          = data.terraform_remote_state.core_infra.outputs.key_vault_url
+  key_vault_name         = data.terraform_remote_state.core_infra.outputs.key_vault_name
+  key_vault_id           = data.terraform_remote_state.core_infra.outputs.key_vault_id
+  acr_login_server       = data.terraform_remote_state.core_infra.outputs.container_registry_login_server
+  acr_id                 = data.terraform_remote_state.core_infra.outputs.container_registry_id
+  storage_container_name = data.terraform_remote_state.core_infra.outputs.storage_container_raw_name
+
   # Shared application identity (from fin_az_core)
-  app_identity_id          = data.terraform_remote_state.core_infra.outputs.app_identity_id
-  app_identity_client_id   = data.terraform_remote_state.core_infra.outputs.app_identity_client_id
+  app_identity_id           = data.terraform_remote_state.core_infra.outputs.app_identity_id
+  app_identity_client_id    = data.terraform_remote_state.core_infra.outputs.app_identity_client_id
   app_identity_principal_id = data.terraform_remote_state.core_infra.outputs.app_identity_principal_id
-  app_identity_tenant_id   = data.terraform_remote_state.core_infra.outputs.app_identity_tenant_id
+  app_identity_tenant_id    = data.terraform_remote_state.core_infra.outputs.app_identity_tenant_id
 }
 
 resource "azurerm_container_app_environment" "c_app_env" {
